dd1a46d254dd1b6ec23dd96eaf6b267a0aea83ba
ariescc <jiachang@mail.nwpu.edu.cn>
2017-12-15 17:23:19 +0800

    RMI 客户端 实现

--- RmiTest/src/com/ariescc/rmi/client/RMIClient.java
+++ RmiTest/src/com/ariescc/rmi/client/RMIClient.java
@@ -1,105 +1,129 @@
 package com.ariescc.rmi.client;
 
-public class RMIClient {
+import com.ariescc.rmi.IRemoteMethod;
+import com.ariescc.rmi.Method;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RmiClient {
+
+    public RmiClient() {}
 
     public static void main(String[] args) {
 
-        if(args.length < 4) {
-            System.out.println("Usage: Command don't exist.");
+        if (args.length < 7) {
+            System.out.println("Usage: command don't exist.");
             System.exit(1);
         }
 
-        if(args[3].equals(Method.register)) {
-            if(args.length != 7) {
-                System.out.println("Usage: java [clientName] [serverName] [portNumber] register " +
-                        "[userName] [password]");
-                System.exit(1);
-            }
-
-            // 调用 Register 方法
+        if (args.length == 7 && args[4].equals(Method.register)) {
             handleRegister(args);
-
-        } else if (args[3].equals(Method.add)) {
-            if(args.length != 11) {
-                System.out.println("Usage: java [clientName] [serverName] [portNumber] add" +
-                        " [username] [password] [otherUsername] [start] [end] [title]");
-                System.exit(1);
-            }
-            System.out.println("Successful!");
-            // 调用 Add 方法
+        } else if (args.length == 11 && args[4].equals(Method.add)) {
             handleAdd(args);
-
-        } else if (args[3].equals(Method.query)) {
-            if(args.length != 9) {
-                System.out.println("Usage: java [clientName] [serverName] [portNumber] query " +
-                        "[username] [password] [start] [end]");
-                System.exit(1);
-            }
-            System.out.println("Successful!");
-            // 调用 Query 方法
+        } else if (args.length == 9 && args[4].equals(Method.query)) {
             handleQuery(args);
-
-        } else if (args[3].equals(Method.delete)) {
-            if(args.length != 8) {
-                System.out.println("Usage: java [clientName] [serverName] [portNumber] delete " +
-                        "[username] [password] [meetingId]");
-                System.exit(1);
-            }
-            System.out.println("Successful!");
-            // 调用 Delete 方法
+        } else if (args.length == 8 && args[4].equals(Method.delete)) {
             handleDelete(args);
+        } else if (args.length == 7 && args[4].equals(Method.clear)) {
+            handleDelete(args);
+        } else {
+            System.out.println("Illegal command. Please check your input!");
+            System.exit(1);
+        }
 
-        } else if (args[3].equals(Method.clear)) {
-            if(args.length != 7) {
-                System.out.println("Usage: java [clientName] [serverName] [portNumber] clear " +
-                        "[username] [password]");
-                System.exit(1);
-            }
-            System.out.println("Successful!");
-            // 调用 Clear 方法
-            handleClear(args);
 
+    }
+
+    private static void handleDelete(String[] args) {
+
+        try {
+
+            // 获得运行 RmiRegister 服务的主机上的注册表
+            Registry registry = LocateRegistry.getRegistry(args[2], Integer.parseInt(args[3]));
+
+            // 查询并获得远程对象的存根
+            IRemoteMethod stub = (IRemoteMethod) registry.lookup("hello");
+
+            // 像在使用本地方法一样调用远程方法
+            String response = stub.sayHello();
+
+            System.out.println("Response: " + response);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
         }
 
     }
 
-    /**
-     * 处理 Clear
-     * @param args
-     */
-    private static void handleClear(String[] args) {
-    }
-
-    /**
-     * 处理 Delete
-     * @param args
-     */
-    private static void handleDelete(String[] args) {
-    }
-
-    /**
-     * 处理 Query
-     * @param args
-     */
     private static void handleQuery(String[] args) {
+
+        try {
+
+            // 获得运行 RmiRegister 服务的主机上的注册表
+            Registry registry = LocateRegistry.getRegistry(args[2], Integer.parseInt(args[3]));
+
+            // 查询并获得远程对象的存根
+            IRemoteMethod stub = (IRemoteMethod) registry.lookup("hello");
+
+            // 像在使用本地方法一样调用远程方法
+            String response = stub.sayHello();
+
+            System.out.println("Response: " + response);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    /**
-     * 处理 Add
-     * @param args
-     */
     private static void handleAdd(String[] args) {
+
+        try {
+
+            // 获得运行 RmiRegister 服务的主机上的注册表
+            Registry registry = LocateRegistry.getRegistry(args[2], Integer.parseInt(args[3]));
+
+            // 查询并获得远程对象的存根
+            IRemoteMethod stub = (IRemoteMethod) registry.lookup("hello");
+
+            // 像在使用本地方法一样调用远程方法
+            stub.Add(args[5], args[6], args[7], args[8], args[9], args[10]);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    /**
-     * 处理 Register
-     * @param args
-     */
     private static void handleRegister(String[] args) {
 
-        // 解析命令
+        try {
 
+            // 获得运行 RmiRegister 服务的主机上的注册表
+            Registry registry = LocateRegistry.getRegistry(args[2], Integer.parseInt(args[3]));
+
+            // 查询并获得远程对象的存根
+            IRemoteMethod stub = (IRemoteMethod) registry.lookup("hello");
+
+            // 像在使用本地方法一样调用远程方法
+            stub.Register(args[5], args[6]);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
 
     }
+
 
 }
