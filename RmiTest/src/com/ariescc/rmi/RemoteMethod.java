4be2b79ee07128d578cedb8160464fa07afd0813
ariescc <jiachang@mail.nwpu.edu.cn>
2017-12-15 17:23:36 +0800

    远程方法实现

--- RmiTest/src/com/ariescc/rmi/RemoteMethod.java
+++ RmiTest/src/com/ariescc/rmi/RemoteMethod.java
@@ -1,22 +1,72 @@
 package com.ariescc.rmi;
 
+import com.ariescc.rmi.server.SaveDataXML;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 /**
- * 创建远程方法接口实现类
+ * IRemoteMethod 实现类
  */
 public class RemoteMethod extends UnicastRemoteObject implements IRemoteMethod {
 
-    // 由于方法参数和返回值最终都将在网络上传输，所以必须是可序列化的。
-    private static final long serialVersionUID = -271947229644133464L;
+    SaveDataXML saveDataXML = new SaveDataXML();
 
     public RemoteMethod() throws RemoteException {
         super();
     }
 
     @Override
-    public String UseRemoteMethod(String name) throws RemoteException {
-        return "Hello" + name;
+    public String sayHello() throws RemoteException {
+        return "hello, world";
     }
+
+    @Override
+    public void Register(String username, String password) throws RemoteException {
+
+        try {
+            saveDataXML.UserRegisterToXML(username, password);
+
+            System.out.println("Successful!");
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void Add(String userOneName, String password, String userTwoName,
+                    String startTime, String endTime, String title) throws RemoteException {
+        try {
+            saveDataXML.MeetingCreateToXML(userOneName, password, userTwoName, startTime, endTime, title);
+
+            System.out.println("Successful!");
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void Query(String username, String password, String startTime,
+                      String endTime) throws RemoteException {
+
+    }
+
+    @Override
+    public void Delete(String username, String password, String meetingId) throws RemoteException {
+
+    }
+
+    @Override
+    public void Clear(String username, String password) throws RemoteException {
+
+    }
+
+
 }
