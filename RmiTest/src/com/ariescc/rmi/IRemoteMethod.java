44551332ef8421ad864c5c74bd0259248a234b88
ariescc <jiachang@mail.nwpu.edu.cn>
2017-12-15 17:23:29 +0800

    远程方法接口

--- RmiTest/src/com/ariescc/rmi/IRemoteMethod.java
+++ RmiTest/src/com/ariescc/rmi/IRemoteMethod.java
@@ -3,11 +3,26 @@ package com.ariescc.rmi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * 创建远程方法接口，该接口继承自 Remote 接口
- */
 public interface IRemoteMethod extends Remote {
 
-    public String UseRemoteMethod(String name) throws RemoteException;
+    // 调用时候发生网络问题或者Server挂掉，抛出 RemoteException
+
+    String sayHello() throws RemoteException;
+
+    // 用户注册
+    void Register(String username, String password) throws RemoteException;
+
+    // 添加会议
+    void Add(String userOneName, String password, String userTwoName, String startTime,
+             String endTime, String title) throws RemoteException;
+
+    // 查询会议
+    void Query(String username, String password, String startTime, String endTime) throws RemoteException;
+
+    // 删除会议：根据会议ID，删除由本用户创建的会议
+    void Delete(String username, String password, String meetingId) throws RemoteException;
+
+    // 清除会议：清除所有本用户创建的会议
+    void Clear(String username, String password) throws RemoteException;
 
 }
