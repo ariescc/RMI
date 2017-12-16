035d48b2c1f53ee6efa965a6bdf3baf7bbd717ad
ariescc <jiachang@mail.nwpu.edu.cn>
2017-12-16 21:53:54 +0800

    用户注册写完，新建会议时间判断未写完

--- RmiTest/src/com/ariescc/rmi/server/SaveDataXML.java
+++ RmiTest/src/com/ariescc/rmi/server/SaveDataXML.java
@@ -1,8 +1,6 @@
 package com.ariescc.rmi.server;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -20,7 +18,7 @@ public class SaveDataXML {
 
     String meetingFile = "Meeting.xml";
 
-    public void MeetingCreateToXML(String userOne, String password, String userTwo,
+    public String MeetingCreateToXML(String userOne, String password, String userTwo,
                                    String startTime, String endTime, String title)
             throws TransformerException, ParserConfigurationException {
 
@@ -30,25 +28,32 @@ public class SaveDataXML {
             initMeetingXML();
         }
 
-        updateMeetingTable(userOne, password, userTwo, startTime, endTime, title);
+        return updateMeetingTable(userOne, password, userTwo, startTime, endTime, title);
 
     }
 
-    private void updateMeetingTable(String userOne, String password, String userTwo,
+    private String updateMeetingTable(String userOne, String password, String userTwo,
                                     String startTime, String endTime, String title) {
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        // 判断会议是否能够成功创建
+        if (judgeMeetingCreateOrNot(userOne, password, userTwo, startTime, endTime)) {
+
+        }
 
         try {
 
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(new File(meetingFile));
 
+            Element root1 = doc.getDocumentElement();
+
             // 创建一个会议的元素结点
             Element eltMeetObj = doc.createElement("MeetObj");
-            Element eltUserOne = doc.createElement("User_1");
-            Element eltpassword = doc.createElement("Password");
-            Element eltUserTwo = doc.createElement("User_2");
+            Element eltUserOne = doc.createElement("User1");
+            Element eltPassword = doc.createElement("Password");
+            Element eltUserTwo = doc.createElement("User2");
             Element eltStartTime = doc.createElement("StartTime");
             Element eltEndTime = doc.createElement("EndTime");
             Element eltTitle = doc.createElement("Title");
@@ -63,7 +68,7 @@ public class SaveDataXML {
 
             // 将文本结点添加为对应元素结点的子节点
             eltUserOne.appendChild(txtUserOne);
-            eltpassword.appendChild(txtPassword);
+            eltPassword.appendChild(txtPassword);
             eltUserTwo.appendChild(txtUserTwo);
             eltStartTime.appendChild(txtStartTime);
             eltEndTime.appendChild(txtEndTime);
@@ -71,11 +76,13 @@ public class SaveDataXML {
 
             // 将 User1 和 User2 和 StartTime 和 EndTime 添加为 Meeting 的子节点
             eltMeetObj.appendChild(eltUserOne);
-            eltMeetObj.appendChild(eltpassword);
+            eltMeetObj.appendChild(eltPassword);
             eltMeetObj.appendChild(eltUserTwo);
             eltMeetObj.appendChild(eltStartTime);
             eltMeetObj.appendChild(eltEndTime);
             eltMeetObj.appendChild(eltTitle);
+
+            root1.appendChild(eltMeetObj);
 
             // 创建 Meeting.XML 文件
             TransformerFactory tff = TransformerFactory.newInstance();
@@ -85,6 +92,9 @@ public class SaveDataXML {
             tf.transform(new DOMSource(doc), new StreamResult(new File(meetingFile)));
 
             tf.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            String response = "会议创建成功！";
+            return response;
 
 
         } catch (ParserConfigurationException e) {
@@ -99,6 +109,220 @@ public class SaveDataXML {
             e.printStackTrace();
         }
 
+        return "程序错误";
+
+    }
+
+    /**
+     *
+     * 判断创建者用户名和密码是否正确
+     *
+     * 判断两个用户是否有其他会议，有其他会议的话是否有时间冲突
+     *
+     * @param userOne 创建者用户名
+     * @param password 创建者密码
+     * @param userTwo 第二个用户的用户名
+     * @param startTime 会议开始时间
+     * @param endTime 会议结束时间
+     * @return
+     */
+    private boolean judgeMeetingCreateOrNot(String userOne, String password,
+                                            String userTwo, String startTime, String endTime) {
+
+        // 判断创建者是否合法，参与者是否合法
+        boolean isUserAvailable = judgeMeetingUserAvailable(userOne, password, userTwo);
+
+        // 判断会议时间是否与参与者其他会议有冲突
+        boolean isTimeAvailable = judgeMeetingTimeAvailable(userOne, userTwo, startTime, endTime);
+
+        // 如果两者均合法
+        if (isUserAvailable && isTimeAvailable) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 判断会议时间是否与参会者的其他会议时间冲突
+     * @param startTime 会议开始时间
+     * @param endTime 会议结束时间
+     * @return
+     */
+    private boolean judgeMeetingTimeAvailable(String userOne, String userTwo, String startTime, String endTime) {
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(userFile));
+
+//            System.out.println(doc == null);
+
+//            System.out.println("doc: " + doc);
+
+            Element root = doc.getDocumentElement();
+
+            // 获得所有 MeetingObj 子节点
+            NodeList childNodes = root.getChildNodes();
+
+            for (int i = 0; i < childNodes.getLength(); i++) {
+
+                Node curNode = childNodes.item(i);
+
+                if ("MeetingObj".equals(curNode.getNodeName())) {
+
+                    // 获取 MeetingObj 下的所有子节点
+                    NodeList nodeDetail = curNode.getChildNodes();
+
+                    for (int j = 0; j < nodeDetail.getLength(); j++) {
+
+                        Node detail = nodeDetail.item(j);
+
+                        if ("User1".equals(detail.getNodeName())) {
+                            if (!userOne.equals(detail.getTextContent())
+                                    && !userTwo.equals(detail.getTextContent())) {
+                                break;
+                            } else {
+
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     * 判断创建者身份是否合法（用户名、密码正确）
+     * 判断参与者用户名是否有效
+     * @param userOne 创建者用户名
+     * @param password 创建者密码
+     * @param userTwo 参与者用户名
+     * @return
+     */
+    private boolean judgeMeetingUserAvailable(String userOne, String password, String userTwo) {
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(userFile));
+
+//            System.out.println(doc == null);
+
+//            System.out.println("doc: " + doc);
+
+            Element root = doc.getDocumentElement();
+
+            // 判断创建者账号密码正确
+            boolean isUserAvailable = judgeUserAvailable(userOne, password);
+
+            // 判断参与者账号有效
+            boolean isParticipantAvailable = judgeUserNameAvaliable(userTwo);
+
+            if (isUserAvailable && isParticipantAvailable) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("程序错误!");
+
+        return false;
+
+    }
+
+    /**
+     * 判断用户用户名密码是否正确
+     * @param userOne 用户名
+     * @param password 密码
+     * @return
+     */
+    private boolean judgeUserAvailable(String userOne, String password) {
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(userFile));
+
+//            System.out.println(doc == null);
+
+//            System.out.println("doc: " + doc);
+
+            Element root = doc.getDocumentElement();
+
+            NodeList childNodes = root.getChildNodes();
+
+            boolean isUserName = false;
+            boolean isPassword = false;
+
+            for (int i = 0; i < childNodes.getLength(); i++) {
+
+                Node curNode = childNodes.item(i);
+
+                if ("UserObj".equals(curNode.getNodeName())) {
+
+                    NodeList nodeDetail = curNode.getChildNodes();
+
+                    for (int j = 0; j < nodeDetail.getLength(); j++) {
+                        Node detail = nodeDetail.item(j);
+
+                        if ("UserName".equals(detail.getNodeName())) {
+                            if (userOne.equals(detail.getTextContent())) {
+                                isUserName = true;
+                            }
+                        } else if ("Password".equals(detail.getNodeName())) {
+                            if (password.equals(detail.getTextContent())) {
+                                isPassword = true;
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+            if (isUserName && isPassword) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("程序错误！");
+
+        return false;
 
     }
 
@@ -127,7 +351,7 @@ public class SaveDataXML {
         Transformer tf = tff.newTransformer();
 
         // 将 document 对象转化成 XML 文件
-        tf.transform(new DOMSource(document), new StreamResult(new File("Meeting.xml")));
+        tf.transform(new DOMSource(document), new StreamResult(new File(meetingFile)));
 
         tf.setOutputProperty(OutputKeys.INDENT, "yes");
 
@@ -142,24 +366,21 @@ public class SaveDataXML {
      * @throws ParserConfigurationException
      * @throws TransformerException
      */
-    public void UserRegisterToXML(String username, String password) throws ParserConfigurationException, TransformerException {
+    public String UserRegisterToXML(String username, String password)
+            throws ParserConfigurationException, TransformerException {
 
         File file = new File(userFile);
 
-        System.out.println(file.exists());
+//        System.out.println(file.exists());
 
         // 如果 UserTable.xml 不存在
         if(!file.exists()) {
+            // 初始化 User.xml 结构和根节点
             initUserXML();
         }
 
-        System.out.println("update begin___");
-
         // 添加一组新的用户节点
-        updateUserTable(username, password);
-
-        System.out.println("update end___");
-
+        return updateUserTable(username, password);
 
     }
 
@@ -168,9 +389,18 @@ public class SaveDataXML {
      * @param username 注册用户名
      * @param password 用户密码
      */
-    private void updateUserTable(String username, String password) {
+    private String updateUserTable(String username, String password) {
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        // 判断该用户名是否可注册
+        if(!judgeUserNameAvaliable(username)) {
+
+            String response = "此用户名已经被注册！";
+
+            return response;
+
+        }
 
         try {
 
@@ -214,6 +444,10 @@ public class SaveDataXML {
 
             tf.setOutputProperty(OutputKeys.INDENT, "yes");
 
+            String response = "注册成功！";
+
+            return response;
+
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (SAXException e) {
@@ -225,6 +459,80 @@ public class SaveDataXML {
         } catch (TransformerException e) {
             e.printStackTrace();
         }
+
+        return "程序错误！";
+
+    }
+
+    /**
+     * 判断用户名是否可注册
+     * @param username 客户端输入的用户名
+     * @return
+     */
+    private boolean judgeUserNameAvaliable(String username) {
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(userFile));
+
+//            System.out.println(doc == null);
+
+//            System.out.println("doc: " + doc);
+
+            Element root = doc.getDocumentElement();
+
+            // 获取所有子节点 UserObj
+            NodeList childNodes = root.getChildNodes();
+
+            // 遍历所有的 UserObj
+            for(int i = 0; i < childNodes.getLength(); i++) {
+
+                // 获得每个位置的节点
+                Node curNode = childNodes.item(i);
+
+                if ("UserObj".equals(curNode.getNodeName())) {
+
+                    // 获取 UserObj 下的子节点
+                    NodeList nodeDetail = curNode.getChildNodes();
+
+                    // 遍历 UserObj 下的子节点
+                    for (int j = 0; j < nodeDetail.getLength(); j++) {
+
+                        // 获取 UserObj 下的每一个子节点
+                        Node detail = nodeDetail.item(j);
+
+                        // 用户名
+                        if ("UserName".equals(detail.getNodeName())) {
+                            if (username.equals(detail.getTextContent())) {
+
+                                System.out.println("用户名已经被注册，不可注册！");
+
+                                return false;
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            System.out.println("用户名未在数据表内出现，可以注册！");
+
+            return true;
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("程序异常");
+
+        return false;
 
     }
 
