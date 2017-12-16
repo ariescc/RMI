1ee3c453214704c2223c3ac8461cd1b5cd082dd7
ariescc <jiachang@mail.nwpu.edu.cn>
2017-12-16 11:57:10 +0800

    Method封装修改

--- RmiTest/src/com/ariescc/rmi/Method.java
+++ RmiTest/src/com/ariescc/rmi/Method.java
@@ -2,10 +2,21 @@ package com.ariescc.rmi;
 
 public enum Method {
 
-    register,
-    add,
-    query,
-    delete,
-    clear,
+    REGISTER("register"),
+    ADD("add"),
+    QUERY("query"),
+    DELETE("delete"),
+    CLEAR("clear");
+
+    private final String text;
+
+    private Method(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 
 }
